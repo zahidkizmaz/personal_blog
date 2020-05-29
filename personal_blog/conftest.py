af3b700729ba538b5a8739fdae477f527ee881ba
Zahid Kizmaz <mzkizmaz@hotmail.com>
2020-05-29 23:27:07 +0200

    Init project

---
+++ personal_blog/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from personal_blog.users.models import User
+from personal_blog.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
